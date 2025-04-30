9b1b46926af4374df15d27a2c3aaee47ad505d17
luigimangione#1fan <pakininninin@gmail.com>
2025-04-30 11:13:43 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -12,7 +12,12 @@ public class APCalendar
    */
   public static int numberOfLeapYears(int year1, int year2)
   { 
-    /* to be implemented in part (a) */
+    int count = 0;
+    for(int i =0;i<=year2-year1;i++){
+      if(isLeapYear(year1+i)
+         count++;
+    }
+    return count;
 
   }
   
@@ -51,6 +56,8 @@ public class APCalendar
    */
   public static int dayOfWeek(int month, int day, int year)
   {
-    /* to be implemented in part (b) */
+    int num = firstDayofYear(year);
+    int days = dayOfYear(month,day,year);
+    return Math.abs(days%7)-num;
   }
 }
